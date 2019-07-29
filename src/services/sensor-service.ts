fb4571d10fefb6bc9e9d010a30c37305d664feff
Torbjörn Vading <torbjorn.vading@nivapro.com>
2019-07-29 21:17:26 +0200

    change cdn for vuetify to 1.5.16

--- src/services/sensor-service.ts
+++ src/services/sensor-service.ts
@@ -4,12 +4,16 @@ import { log } from '@/services/logger';
 
 import { Data, Descriptor, Sensor } from '@/models/sensors';
 
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
+
+const io: AxiosInstance = axios.create({
+        headers: {'Content-Type': 'application/json'},
+      });
 
 export function getSensors(from: number = 1): Promise<Sensor[]> {
         return new Promise<Sensor[]> ((resolve, reject) => {
                 const url = iTemperAPI + '/sensors?from=' + from;
-                axios.get(url)
+                io.get(url)
                 .then((response) => {
                         const data: Sensor[] = response.data.slice();
                         // log.debug('axios - response.data: ', data);
@@ -22,7 +26,7 @@ export function getSensorSamples(desc: Descriptor): Promise<Data[]> {
         return new Promise<Data[]> ((resolve, reject) => {
                 const url = iTemperAPI + '/sensors' + '/' + desc.SN + '/' + desc.port;
                 // log.debug('getTrend', url);
-                axios.get(url)
+                io.get(url)
                 .then((response) => {
                         const data: Data[] = response.data.slice();
                         // log.debug('axios - response.data: ', data);
