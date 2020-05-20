13de1e38d6af54f5c1b9a89a8d1d4a4e45b81d99
Torbjörn Vading <torbjorn.vading@nivapro.com>
2020-05-20 20:15:12 +0200

    Fix Admin reset, setters and getters

--- src/store/admin.ts
+++ src/store/admin.ts
@@ -13,19 +13,28 @@ export class Admin {
     public get level(): LogLevel {
         return this.mLevel;
     }
-
     public set level(value: LogLevel) {
-        this.mLevel = value;
+        Vue.set(this, 'mLevel', value);
     }
-
+    public get error(): string {
+        return this.mError;
+    }
+    public set error(value: string) {
+        Vue.set(this, 'mError', value);
+    }
+    public reset() {
+        this.level = LogLevel.error;
+        this.error = '';
+    }
     public resetLevel(): Promise<boolean>  {
         return new Promise ((resolve, reject) => {
             this.adminService.setLogLevel(LogLevel.error)
             .then((updated: boolean) => {
                 if (!updated) {
-                    reject({status: 94, message: 'Log level could not be reset'});
+                    this.error =  'Log level could not be reset';
+                    reject({status: 94, message: this.error});
                 } else {
-                    Vue.set(this, 'mLevel', LogLevel.error);
+                    this.level = LogLevel.error;
                     resolve(true);
                 }
             })
@@ -39,9 +48,10 @@ export class Admin {
             this.adminService.setLogLevel(newLevel)
             .then((updated: boolean) => {
                 if (!updated) {
-                    reject({status: 94, message: 'Log level could not be updated'});
+                    this.error =  'Log level could not be updated';
+                    reject({status: 94, message: this.error});
                 } else {
-                    Vue.set(this, 'mLevel', newLevel);
+                    this.level = newLevel;
                     resolve(true);
                 }
             })
@@ -49,6 +59,6 @@ export class Admin {
         });
     }
     private resetError() {
-        this.mError = '';
+        this.error = '';
     }
 }
