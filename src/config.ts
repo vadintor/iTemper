c5ffbecc02e06998ce716caf78d0dd662ac96185
Torbjörn Vading <torbjorn.vading@nivapro.com>
2020-06-21 05:21:26 +0200

    Configure URLs based on process.env (#16)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,27 @@
-// export const iTemperAPI = 'http://localhost:3000/';
-// export const iTemperWS = 'ws://localhost:3000/ws';
+interface Config {
+    development: boolean;
+    iTemperAPI: string;
+    iTemperWS: string;
+}
 
-export const iTemperAPI = 'https://api.itemper.io/';
-export const iTemperWS = 'wss://api.itemper.io/ws';
+function configure(): Config {
+    const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+    if (development) {
+        return {
+            development,
+            iTemperAPI: 'http://localhost:3000/',
+            iTemperWS: 'ws://localhost:3000/ws',
+        };
+    } else {
+        return {
+            development,
+            iTemperAPI: 'https://api.itemper.io/',
+            iTemperWS: 'wss://api.itemper.io/ws',
+        };
+    }
+
+}
+
+export const config = configure();
+export const iTemperAPI = config.iTemperAPI;
+export const iTemperWS = config.iTemperWS;
