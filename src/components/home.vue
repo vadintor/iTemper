92554992122d10b6ed00ad3bd3f8157827f80031
Torbjörn Vading <torbjorn.vading@nivapro.com>
2020-06-21 08:47:19 +0200

    Refactor Admin into sub components

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,12 +1,24 @@
 
 <template>
-    <h1>Välkommen till itemper.io</h1>
+    <div>
+        <h1>Välkommen till itemper.io</h1>
+        <admin-config-card></admin-config-card>
+    </div>
 </template>
 <script lang='ts'>
-import { Vue, Component} from 'vue-property-decorator';
 
+// Config
+import {config} from '@/config';
+
+// Services
 import { log } from '@/services/logger';
-@Component({})
+
+// Vue
+import { Vue, Component} from 'vue-property-decorator';
+import AdminConfigCard from './admin-config-card.vue';
+@Component({components: {
+    AdminConfigCard,
+}})
 export default class Home extends Vue {
 
 }
